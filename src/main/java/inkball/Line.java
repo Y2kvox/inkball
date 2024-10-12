124889700acad23d73cff88ca3fad4d2b42e8814
Y2kvox <abraryasserolevel2@gmail.com>
2024-10-12 20:21:59 +1100

    Line class added

---
+++ src/main/java/inkball/Line.java
@@ -0,0 +1,12 @@
+package inkball;
+
+import processing.core.*;
+class Line {
+    PVector start;
+    PVector end;
+
+    Line(PVector start, PVector end) {
+        this.start = start;
+        this.end = end;
+    }
+}
